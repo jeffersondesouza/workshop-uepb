90298d1336a1b86c5092a8ecbfbcae47aa90a7f6
jefferson <jeffersondesouzacordel@gmail.com>
2018-11-14 17:44:09 -0200

    change image

---
+++ static-app/index.js
@@ -0,0 +1,64 @@
+const img1 = "https://images.punkapi.com/v2/keg.png";
+const img2 = "https://images.punkapi.com/v2/6.png";
+const img3 = "https://images.punkapi.com/v2/2.png";
+const img4 = "https://images.punkapi.com/v2/9.png";
+const img5 = "https://images.punkapi.com/v2/8.png";
+const img6 = "https://images.punkapi.com/v2/10.png";
+const img7 = "https://images.punkapi.com/v2/6.png";
+
+const cervejaSelectedElement = document.getElementById('js-img-selected');
+
+
+document.getElementById('c1')
+  .addEventListener('click', function () {
+    console.log();
+    cervejaSelectedElement.src = img1;
+  });
+
+document.getElementById('c2')
+  .addEventListener('click', function () {
+    console.log();
+    cervejaSelectedElement.src = img2;
+  });
+
+
+document.getElementById('c3')
+  .addEventListener('click', function () {
+    console.log();
+    cervejaSelectedElement.src = img3;
+  });
+
+
+document.getElementById('c4')
+  .addEventListener('click', function () {
+    console.log();
+    cervejaSelectedElement.src = img4;
+  });
+
+
+document.getElementById('c5')
+  .addEventListener('click', function () {
+    console.log();
+    cervejaSelectedElement.src = img5;
+  });
+
+
+document.getElementById('c6')
+  .addEventListener('click', function () {
+    console.log();
+    cervejaSelectedElement.src = img6;
+  });
+
+
+document.getElementById('c7')
+  .addEventListener('click', function () {
+    console.log();
+    cervejaSelectedElement.src = img7;
+  });
+
+
+
+
+
+
+
